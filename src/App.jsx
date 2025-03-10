313cbc941ce4fa8c6d5b40f29b37422c2b219570
dev78ashish <dev78ashish@gmail.com>
2025-03-10 23:23:25 +0530

    context added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,46 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import ResumeParser from './ResumeParser'
 import LandingPage from './Pages/LandingPage'
 import { LogIn } from 'lucide-react'
 import Login from './Pages/Login'
 import Signup from './Pages/Signup'
 import ResumeParser from './ResumeParser'
+import { AuthProvider } from './context/AuthContext';
+import Navbar from './Components/Navbar';
+import ProtectedRoute from './Components/ProptectedRoute';
+import Dashboard from './Pages/Dashboard';
+
 
 const App = () => {
 
   useEffect(() => {
-  //   fetch('http://localhost:8000/public/yes').then(response => response.text()).then(data => {
-  //     console.log(data)
-  // })
+    //   fetch('http://localhost:8000/public/yes').then(response => response.text()).then(data => {
+    //     console.log(data)
+    // })
   }, [])
 
   return (
     <div>
-      <ResumeParser />
+      <AuthProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/landingpage' element={<LandingPage />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+
+            <Route
+              path='/'
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Router>
+      </AuthProvider>
     </div>
   )
 }
